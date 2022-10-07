ed8397cb59d6d6e4b08f379c54c434a3c6c538b8
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2022-10-07 12:50:24 +0700

    .

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,9 +1,9 @@
 import "../styles/one-notification.scss";
 
-import oneNotification from "./one-notification";
+import oneTemplate from "./one-template";
 import oneObjectToStyle from "./one-objectToStyle";
 import oneInject from "./one-inject";
-import oneTemplate from "./one-template";
+import oneNotification from "./one-notification";
 
 export default oneNotification;
 
